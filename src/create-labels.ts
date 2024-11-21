2675471861524194c2ca272e6cb45b80c960087c
Andrew Lisowski <lisowski54@gmail.com>
2024-11-20 21:56:09 -0800

    init db

---
+++ src/create-labels.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { addUserLabel } from "./label-server.js";
+
+const data = fs.readFileSync("contributions.json", "utf8");
+const map = JSON.parse(data);
+
+for (const [key, value] of Object.entries(map) as [
+  string,
+  { industries: string[]; contributors: string[] }
+][]) {
+  console.log(`Adding labels for ${key}`);
+
+  for (const industry of value.industries.slice(0, 3)) {
+    await addUserLabel(key, {
+      name: industry,
+      description: `This representative is funded by the "${industry}" industry.`,
+    });
+  }
+
+  for (const contributor of value.contributors.slice(0, 3)) {
+    await addUserLabel(key, {
+      name: contributor,
+      description: `This representative has funded by "${contributor}"`,
+    });
+  }
+}
