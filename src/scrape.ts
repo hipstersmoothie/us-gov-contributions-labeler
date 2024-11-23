73abca0531428039f7adbe51f3d6397eddaeeaba
Andrew Lisowski <lisowski54@gmail.com>
2024-11-22 17:44:02 -0800

    add dwight

--- src/scrape.ts
+++ src/scrape.ts
@@ -119,4 +119,4 @@ async function scrapeDataForHandle(handle: string) {
 
 // scrapeDataForList(`at://${process.env.DID}/app.bsky.graph.list/3lbgx3lqlwk2d`);
 
-scrapeDataForHandle("rephuffman.bsky.social");
+scrapeDataForHandle("repdwightevans.bsky.social");
