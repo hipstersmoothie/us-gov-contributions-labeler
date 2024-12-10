28d48dc1478e47753dc37b3f4510724c342e217b
Andrew Lisowski <lisowski54@gmail.com>
2024-12-10 14:33:16 -0800

    add zoelofgren.bsky.social

--- src/scrape.ts
+++ src/scrape.ts
@@ -120,4 +120,4 @@ async function scrapeDataForHandle(handle: string) {
 
 // scrapeDataForList(`at://${process.env.DID}/app.bsky.graph.list/3lbgx3lqlwk2d`);
 
-scrapeDataForHandle("kaine.senate.gov");
+scrapeDataForHandle("zoelofgren.bsky.social");
